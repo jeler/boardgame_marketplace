0ea398737752d226378e3dd4d18bdacb3047250b
Jennifer Eler <jennifereler@Jennifers-MacBook-Pro.local>
2019-07-02 21:35:08 -0500

    remove brcypt-as-promised

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 var express = require("express"),
-    app = express(),
-    session = require('express-session'),
-    bodyParser = require('body-parser'),
-    path = require('path');
-    mongoose = require('mongoose')    
-    
-app.use(session({ secret: 'codingdojorocks' }));
+  app = express(),
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  path = require("path");
+mongoose = require("mongoose");
 
-app.use(bodyParser.json())
+app.use(session({ secret: "codingdojorocks" }));
 
-bcrypt = require('bcrypt-as-promised')
-    
-require('./server/config/mongoose.js')
-    
-require('./server/config/routes.js')(app)
+app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+bcrypt = require("bcryptjs");
+
+require("./server/config/mongoose.js");
+
+require("./server/config/routes.js")(app);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.all("*", (req, res, next) => {
-    res.sendFile(path.resolve("./client/dist/index.html"))
-})
+  res.sendFile(path.resolve("./client/dist/index.html"));
+});
 
 // app.listen(1337, function () {
 //     console.log("listening on port 1337");
